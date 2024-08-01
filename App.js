8e82c355ff10a6885a9e404905eb884ce7bfa820
NS027 <chris_n89@hotmail.com>
2024-08-01 00:38:27 -0700

    Refactor express server to use separate module for routes

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 // const express = require('express');
 import express from 'express';
+import Hello from './hello.js';
+
 const app = express();
-
-app.get("/hello", (req, res) => {
-    res.send("Hello World!");
-})
-
+Hello(app);
 
 app.listen(4000); 
