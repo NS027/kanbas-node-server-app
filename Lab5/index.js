a067671421ff1a29ccf1d0cfb75b19099b1d8c52
NS027 <chris_n89@hotmail.com>
2024-08-02 00:06:33 -0700

    Add WorkingWithArrays module to express server

--- Lab5/index.js
+++ Lab5/index.js
@@ -1,5 +1,6 @@
 import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
+import WorkingWithArrays from "./WorkingWithArrays.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
 const Lab5 = (app) => {
     app.get("/lab5/welcome", (req, res) => {
@@ -11,6 +12,7 @@ const Lab5 = (app) => {
     PathParameters(app);
     QueryParameters(app);
     WorkingWithObjects(app);
+    WorkingWithArrays(app);
 
 };
 export default Lab5;
