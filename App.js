ecd95de8f505bc25a37df1cbac401b579fa74a0c
NS027 <chris_n89@hotmail.com>
2024-08-04 01:17:19 -0700

    add assignment server

--- App.js
+++ App.js
@@ -4,13 +4,16 @@ import Hello from './hello.js';
 import Lab5 from './Lab5/index.js';
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import AssignmentRoutes from './Kanbas/Assignment/routes.js';
 import cors from 'cors';
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
+AssignmentRoutes(app);
 Hello(app);
 Lab5(app);
 
