7d9727cbb5e41d8f8d564e908f071b4e0bd3c79b
NS027 <chris_n89@hotmail.com>
2024-08-03 20:37:16 -0700

    create the course server

--- App.js
+++ App.js
@@ -2,11 +2,13 @@
 import express from 'express';
 import Hello from './hello.js';
 import Lab5 from './Lab5/index.js';
+import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from 'cors';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+CourseRoutes(app);
 Hello(app);
 Lab5(app);
 
