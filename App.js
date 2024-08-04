b22435a33143d006a57404a1672226eab6088dc7
NS027 <chris_n89@hotmail.com>
2024-08-03 22:59:54 -0700

    create module server

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import express from 'express';
 import Hello from './hello.js';
 import Lab5 from './Lab5/index.js';
 import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import cors from 'cors';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 CourseRoutes(app);
+ModuleRoutes(app);
 Hello(app);
 Lab5(app);
 
