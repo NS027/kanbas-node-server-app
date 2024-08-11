c2bb7f2ea1035fea77eb3212779f27ed68e9d994
NS027 <chris_n89@hotmail.com>
2024-08-11 00:05:11 -0700

    create User in Mongodb

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 // const express = require('express');
+import "dotenv/config";
 import express from 'express';
 import Hello from './hello.js';
 import Lab5 from './Lab5/index.js';
@@ -6,7 +7,12 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from './Kanbas/Assignment/routes.js';
 import cors from 'cors';
+import UserRoutes from './User/routes.js';
 
+import mongoose from 'mongoose';
+const CONNECTION_STRING = 
+    process.env.MONGO_CONNECTION_STRING ||'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
@@ -16,5 +22,6 @@ ModuleRoutes(app);
 AssignmentRoutes(app);
 Hello(app);
 Lab5(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000); 
